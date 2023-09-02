383a419390f75c2a5b6c742843953fd0f961c956
Jason <2923280915@qq.com>
2023-09-02 15:50:45 +0800

    初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  esbuild: {},
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
-    host: true,
-    port: 8080,
     open: true,
+    host: '0.0.0.0',
+    port: 8080,
   }
 })
